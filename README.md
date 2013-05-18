287d33f1fc43efcab5b5e7ce26c3c9ccabeaae79
Matt Neundorf <mn@ninevillage.com>
2013-05-18 17:11:00 +0200

    update

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 ceftool
 =======
 
-To create cef projects really easy
+To create cef projects really easy.
+
+
+## Installation ##
+
+```
+coming soon (npm install ceftool)
+```
+
+## Usage ##
+
+```
+coming soon (ceftool MyCEFProject --without --htmlarchive)
+```
+
---
+++ ceftool/ceftool/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ceftool
+//
+//  Created by Matt Neundorf on 16.05.13.
+//  Copyright (c) 2013 Ninevillage. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
